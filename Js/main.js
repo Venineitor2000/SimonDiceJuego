89908a2204ee05e036b161775736a86311a7d352
Venineitor2000 <altunajuan@gmail.com>
2023-08-06 22:46:44 -0300

    Cambiamos el main para que se ejecute cuando se cargue el dom

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,9 @@
-var botonesJugables = [];
-var boton1 = new BotonJugable("botonJuego1", "imagenApretada1.png", "imagenApagada1.png", "imagenEncendida1.png");
-var boton2 = new BotonJugable("botonJuego2", "imagenApretada2.png", "imagenApagada2.png", "imagenEncendida2.png");
-var boton3 = new BotonJugable("botonJuego3", "imagenApretada3.png", "imagenApagada3.png", "imagenEncendida3.png");
-var boton4 = new BotonJugable("botonJuego4", "imagenApretada4.png", "imagenApagada4.png", "imagenEncendida4.png");
-
-botonesJugables.push(boton1, boton2, boton3, boton4);
-/*<button id="botonPlay" onclick = "BotonPlay()" onmousedown = "Boton('botonPlay', 'botonPlayApretado.png', ValidarBotonPlayInteractauble)" onmouseup="Boton('botonPlay', 'botonPlay.png', ValidarBotonPlayInteractauble)" onmouseleave="Boton('botonPlay', 'botonPlay.png', ValidarBotonPlayInteractauble)"
-                >
-                    <img src="Sprites/botonPlay.png" alt="Boton play">
-            </button>
-            */
+document.addEventListener("DOMContentLoaded", function(){
+    var botonesJugables = [];
+    var boton1 = new BotonJugable("botonJuego1", "imagenApretada1.png", "imagenApagada1.png", "imagenEncendida1.png");
+    var boton2 = new BotonJugable("botonJuego2", "imagenApretada2.png", "imagenApagada2.png", "imagenEncendida2.png");
+    var boton3 = new BotonJugable("botonJuego3", "imagenApretada3.png", "imagenApagada3.png", "imagenEncendida3.png");
+    var boton4 = new BotonJugable("botonJuego4", "imagenApretada4.png", "imagenApagada4.png", "imagenEncendida4.png");
+    
+    botonesJugables.push(boton1, boton2, boton3, boton4);
+});
